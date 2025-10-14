37585242e7ba7df394e926d45dce622c6ed010a3
Santoshpatel100 <your-correct-email@example.com>
2025-10-14 09:52:27 +0530

    signUpSchema

--- src/schemas/SignupSchema.ts
+++ src/schemas/SignupSchema.ts
@@ -6,3 +6,11 @@ export const usernamevalidation=z
 .max(20,'must be no more then 20 char')
 .regex(/^[a-zA-Z0-9_]+$/,'Username can only contain letters, numbers, and underscores')
 .trim();
+
+export const SignUpSchema=z.object({
+    username:usernamevalidation,
+    email:z.string().email({message:'Invalid email address'}).max(100,'Email must be no more than 100 characters long').trim(),
+    password:z.string().min(6,'Password must be at least 6 characters long').max(100,'Password must be no more than 100 characters long'),
+    
+})
+
