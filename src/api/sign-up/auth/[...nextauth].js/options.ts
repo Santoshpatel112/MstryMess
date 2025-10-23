7c84e599f13088d00340fb01f5af44b78d87c427
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-10-23 10:24:29 +0530

    Fix duplicate options file or remove legacy options

---
+++ src/api/sign-up/auth/[...nextauth].js/options.ts
@@ -0,0 +1,73 @@
+import { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcryptjs';
+import dbconnect from '@/lib/dbConnect';
+import UserModel from '@/Model/User';
+import { use } from 'react';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials:any): Promise<any> {
+        if (!credentials?.email || !credentials?.password) return null;
+
+        try {
+          await dbconnect(); // connect to the database
+          const user = await UserModel.findOne({ email: credentials.email }).exec(); // find user by email
+          if (!user) return null;
+          if(user.isveried === false) throw new Error('Email not verified');
+          const isPasswordValid = await bcrypt.compare(credentials.password, user.password);
+          if (!isPasswordValid) return null;
+          return user;
+        } catch (err) {
+          console.error('Authorize error', err);
+          return null;
+        }
+      }
+    })
+  ],
+// define session strategy and max age
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60
+  },
+ 
+   // define JWT and session callbacks
+  callbacks: {
+    async jwt({ token, user}) {
+        if(user){
+            token._id=user._id?.toString();
+            token.isveried=user.isveried;
+            token.isAccepting=user.isAccepting;
+            token.username=user.username;
+
+        }
+
+      return token
+    },
+    async session({ session,  token }) {
+        if(token){
+            session.user._id=token._id?.toString();
+            session.user.isveried=token.isveried;
+            session.user.isAccepting=token.isAccepting;
+            session.user.username=token.username;
+        }
+      return session
+    },
+    
+  },
+// specify custom pages for sign-in and error handling
+  pages: {
+    signIn: '/sign-in',
+    error: '/auth/error'
+  },
+
+  secret: process.env.NEXTAUTH_SECRET
+};
+
+export default authOptions;
