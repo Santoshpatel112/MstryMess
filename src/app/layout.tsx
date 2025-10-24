11edfd0efade064018fcdf0edc727e24bf368d25
Santoshpatel112 <santoshpatelvns5@gmail.com>
2025-10-24 11:03:29 +0530

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Authprovider from "../context/AuthProvider";
+import AuthProvider from "../context/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Authprovider>
+        <AuthProvider>
           {children}
-        </Authprovider>
+        </AuthProvider>
       </body>
     </html>
   );
